a8e0c9c4723dcb1199bb1117610bfef46405fa6e
wg001 <1044070679@qq.com>
2024-06-05 09:20:32 +0800

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,20 @@
 import 'dart:developer';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
+        backgroundColor: Colors.orange,
       ),
       floatingActionButton: FloatingActionButton(
         key: ValueKey('add todo'),
@@ -33,89 +39,38 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 );
-              }
-          );
+              });
         },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Text("Welcome to the Home Page!"),
-            Stack(
-              children: [
-                const Padding(
-                  //padding: EdgeInsets.all(8.0),
-                  padding: const EdgeInsets.only(
-                    left: 8.0,
-                    right: 30.0, // 增加右侧填充，确保图标不覆盖文本
-                    top: 8.0,
-                    bottom: 8.0,
-                  ),
-                  child: Text(
-                    'Welcome to the Home Page!',
-                    style: TextStyle(fontSize: 12),
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-
-                  child: GestureDetector(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('Details'),
-                            content: Text('This is the detail information.'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop(); // 关闭对话框
-                                },
-                                child: Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    child: Icon(
-                      Icons.info_outline,
-                      color: Colors.blue,
+      body: Container(
+        color: Colors.orange,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                  // height: 0.4 * MediaQuery.of(context).size.height,
+                  height: screenHeight * 0.4,
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.bug_report, color: Colors.black),
+                          title: Text(
+                            "Report an Issue",
+                            key: ValueKey(''),
+                          ),
+                          subtitle: Text(
+                            "Having an issue ? Report it here",
+                            key: ValueKey(''),
+                          ),
+                          //onTap: () => launchURL(ISSUE_URL)),
+                        )
+                      ],
                     ),
-                  ),
-                ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                log("details");
-                Map<String, String> args = {
-                  'id': '32',
-                  'phone': '12442',
-                  'email': 'aksdf@gmail.com',
-                };
-                Router
-                    .of(context)
-                    .routerDelegate
-                    .setNewRoutePath(
-                    RouteSettings(name: '/details', arguments: args));
-              },
-              child: Text('Go to Details Page'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                log("settings");
-                Router
-                    .of(context)
-                    .routerDelegate
-                    .setNewRoutePath(RouteSettings(name: '/settings'));
-              },
-              child: Text('Go to Settings Page'),
-            ),
-          ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
