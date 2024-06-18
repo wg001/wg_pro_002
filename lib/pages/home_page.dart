41e5190199b835f7a78895e3edbba43b33ddcf39
wanggang <1044070679@qq.com>
2024-06-18 17:56:42 +0800

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:wg_pro_002/redux/wg_state.dart';
 import 'package:wg_pro_002/utils/logger_util.dart';
 import 'package:wg_pro_002/utils/navigator_utils.dart';
 
@@ -12,6 +15,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+    //  Store<WGState> store = StoreProvider.of(context);
+    Store<WGState> store = StoreProvider.of(context);
+   bool? isLoggedIn = store.state.login??false;
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
@@ -56,10 +62,10 @@ class HomePage extends StatelessWidget {
                     leading: Icon(Icons.bug_report, color: Colors.black),
                     title: Text("Report an Issue"),
                     subtitle: Text("Having an issue? Report it here"),
-                    trailing: ElevatedButton(
-                      onPressed: ()=>navigateToLogin(context),
+                   trailing: !isLoggedIn?ElevatedButton(
+                      onPressed: () => navigateToLogin(context),
                       child: Text('Login'),
-                    ),
+                    ):null,
                   ),
                 ),
               ),
