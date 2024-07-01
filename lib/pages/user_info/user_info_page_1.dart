d87e1ec9db4258edde81801089fa0f821ec148fb
wg001 <1044070679@qq.com>
2024-07-02 02:59:49 +0800

    update

--- lib/pages/user_info/user_info_page_1.dart
+++ lib/pages/user_info/user_info_page_1.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:wg_pro_002/app/model/UserInfo.dart';
 import 'package:wg_pro_002/dao/dao_result.dart';
 import 'package:wg_pro_002/dao/user_dao.dart';
+import 'package:wg_pro_002/utils/common_utils.dart';
 import 'package:wg_pro_002/widget/custom_dropdown.dart';
 import 'package:wg_pro_002/widget/input_widget.dart';
+
+const TextStyle InputStyle = TextStyle(
+  color: Colors.grey, // 设置 hintText 的颜色
+  fontSize: 16.0,
+);
 
 class UserInfoPage1 extends StatefulWidget {
   static const String sName = "userInfoPage1";
@@ -21,15 +28,48 @@ class _UserInfoPage1State extends State<UserInfoPage1>
 
   late UserInfo userInfo;
 
-  @override
-  Future<void> didChangeDependencies() async {
-    super.didChangeDependencies();
+  late TextEditingController idController;
+  late TextEditingController firstController;
 
+  bool isLoading = true; // 添加一个加载状态标志
+
+  final _formKey = GlobalKey<FormState>(); // 创建一个 Form 的 GlobalKey
+
+  @override
+  void initState() {
+    super.initState();
+    idController = TextEditingController();
+    firstController = TextEditingController();
+    // 模拟异步加载数据
+    _loadUserData();
+  }
+
+  @override
+  void dispose() {
+    idController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _loadUserData() async {
     DataResult res = await UserDao.getUserInfo();
     if (res.result && res.data is UserInfo) {
-      userInfo = res.data as UserInfo;
+      setState(() {
+        UserInfo userInfo = res.data as UserInfo;
+        idController.text = userInfo.idNo ?? ''; // 假设 userInfo 有一个 id 字段
+        firstController.text = userInfo.firstName ?? '';
+        isLoading = false; // 数据加载完成
+      });
     }
   }
+  // @override
+  // Future<void> didChangeDependencies() async {
+  //   super.didChangeDependencies();
+
+  //   DataResult res = await UserDao.getUserInfo();
+  //   if (res.result && res.data is UserInfo) {
+  //     userInfo = res.data as UserInfo;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -41,9 +81,16 @@ class _UserInfoPage1State extends State<UserInfoPage1>
         maxLines: 1,
         overflow: TextOverflow.ellipsis,
       )),
-      body: Column(children: <Widget>[
-        buildTable(),
-      ]),
+      body: isLoading
+          ? CircularProgressIndicator()
+          : Form(
+              key: _formKey,
+              child: Column(
+                  //  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    buildTable(),
+                    SubmitButton(),
+                  ])),
     );
   }
 
@@ -51,7 +98,7 @@ class _UserInfoPage1State extends State<UserInfoPage1>
     return Table(
       columnWidths: const <int, TableColumnWidth>{
         0: FlexColumnWidth(1),
-        1: FlexColumnWidth(1),
+        1: FlexColumnWidth(2),
       },
       border: TableBorder(
         horizontalInside: BorderSide(
@@ -61,33 +108,123 @@ class _UserInfoPage1State extends State<UserInfoPage1>
           style: BorderStyle.solid, // Style of the line
         ),
       ),
-      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      // defaultVerticalAlignment: TableCellVerticalAlignment.,
+
       children: [
-        buildTableRow('Please Input Your ID', InputWidget()),
+        buildTableRow(
+          'ID No.',
+          InputWidget(
+            hintText: "Enter your ID",
+            controller: idController,
+            cursorHeight: 20,
+            textStyle: TextStyle(height: 1.5),
+            errorStyle: TextStyle(
+              height: 0, // 减小错误文本的高度影响
+              color: Colors.transparent, // 使其透明
+            ),
+            border: OutlineInputBorder(
+              // 设置边框的颜色和宽度
+              borderSide:
+                  BorderSide(color: Colors.grey.withOpacity(0.5), width: 0.5),
+              // 设置边框的圆角
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                // Fluttertoast.showToast(
+                //     msg: "Please enter your ID number", // Toast message
+                //     toastLength:
+                //         Toast.LENGTH_SHORT, // Duration for message display
+                //     gravity: ToastGravity.CENTER, // Position of toast message
+                //     timeInSecForIosWeb: 1, // Duration in seconds for iOS
+                //     backgroundColor:
+                //         Colors.red, // Background color for toast message
+                //     textColor: Colors.white, // Text color
+                //     fontSize: 16.0 // Font size
+                //     );
+                CommonUtils.showToast('Please enter your ID number');
+                return '';
+              }
+              return null; // 无错误时返回null
+            },
+          ),
+        ),
+
+        buildTableRow(
+          'First Name',
+          InputWidget(
+            hintText: "Enter your First Name",
+            controller: firstController,
+            cursorHeight: 20,
+            textStyle: TextStyle(height: 1.5),
+            errorStyle: TextStyle(
+              height: 0, // 减小错误文本的高度影响
+              color: Colors.transparent, // 使其透明
+            ),
+            border: OutlineInputBorder(
+              // 设置边框的颜色和宽度
+              borderSide:
+                  BorderSide(color: Colors.grey.withOpacity(0.5), width: 0.5),
+              // 设置边框的圆角
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                // Fluttertoast.showToast(
+                //     msg: "Please enter your ID number", // Toast message
+                //     toastLength:
+                //         Toast.LENGTH_SHORT, // Duration for message display
+                //     gravity: ToastGravity.CENTER, // Position of toast message
+                //     timeInSecForIosWeb: 1, // Duration in seconds for iOS
+                //     backgroundColor:
+                //         Colors.red, // Background color for toast message
+                //     textColor: Colors.white, // Text color
+                //     fontSize: 16.0 // Font size
+                //     );
+                CommonUtils.showToast('Please enter your First Name');
+                return '';
+              }
+              return null; // 无错误时返回null
+            },
+          ),
+        ),
+
         // buildTableRow('Please Select Gender', CustomDropdown(null,)),
       ],
     );
   }
 
-  TableRow buildTableRow(String leftText, Widget item,
-      {TextAlign align = TextAlign.center}) {
+  TableRow buildTableRow(String leftText, Widget item) {
     return TableRow(
       children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: SizedBox(
-              height: 50,
-              child: Center(
-                  child: Text(
-                leftText,
-                textAlign: align,
-              ))),
+        Container(
+          height: 40,
+          padding: EdgeInsets.only(left: 8.0, top: 5), // 水平内边距
+          alignment: Alignment.centerLeft, // 水平靠左
+          child: Text(leftText),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
+        Container(
+          height: 40,
+          padding: EdgeInsets.only(right: 8.0, top: 5), // 水平内边距
+          alignment: Alignment.centerLeft, // 水平靠左
           child: item,
         ),
       ],
+    );
+  }
+
+  Widget SubmitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState?.validate() ?? false) {
+          // 表单验证成功
+          print('Form is valid');
+        } else {
+          // 表单验证失败
+          print('Form is not valid');
+        }
+      },
+      child: Text("Submit"),
     );
   }
 }
--- lib/widget/input_widget.dart
+++ lib/widget/input_widget.dart
@@ -1,27 +1,37 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-/// 带图标的输入框
 class InputWidget extends StatefulWidget {
   final bool obscureText;
-
   final String? hintText;
-
   final IconData? iconData;
-
   final ValueChanged<String>? onChanged;
-
   final TextStyle? textStyle;
-
+  final TextStyle? hintStyle;
   final TextEditingController? controller;
+  final InputBorder? border;
+  final double? cursorHeight;
+  final InputBorder? focusedBorder;
+  final TextStyle? errorStyle;
+  final InputBorder? enabledBorder;
+  final String? Function(String?)? validator; // 添加验证器函数参数
 
-  const InputWidget(
-      {super.key,
-      this.hintText,
-      this.iconData,
-      this.onChanged,
-      this.textStyle,
-      this.controller,
-      this.obscureText = false});
+  const InputWidget({
+    Key? key,
+    this.hintText,
+    this.hintStyle,
+    this.iconData,
+    this.onChanged,
+    this.textStyle,
+    this.controller,
+    this.obscureText = false,
+    this.border,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.errorStyle,
+    this.cursorHeight,
+    this.validator, // 接收一个验证函数
+  }) : super(key: key);
 
   @override
   _InputWidgetState createState() => _InputWidgetState();
@@ -30,20 +40,27 @@ class InputWidget extends StatefulWidget {
 class _InputWidgetState extends State<InputWidget> {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-        controller: widget.controller,
-        onChanged: widget.onChanged,
-        obscureText: widget.obscureText,
-        decoration: InputDecoration(
-          hintText: widget.hintText,
-          icon: widget.iconData == null ? null : Icon(widget.iconData),
-        ),
-        magnifierConfiguration: TextMagnifierConfiguration(magnifierBuilder: (
-          BuildContext context,
-          MagnifierController controller,
-          ValueNotifier<MagnifierInfo> magnifierInfo,
-        ) {
-          return null;
-        }));
+    return TextFormField(
+      controller: widget.controller,
+      onChanged: widget.onChanged,
+      obscureText: widget.obscureText,
+      style: widget.textStyle,
+      textAlignVertical: TextAlignVertical.bottom,
+      cursorHeight: widget.cursorHeight,
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        errorStyle: widget.errorStyle ?? widget.errorStyle,
+        hintStyle: widget.hintStyle == null
+            ? TextStyle(color: Colors.grey.withOpacity(0.5))
+            : widget.hintStyle,
+        icon: widget.iconData == null ? null : Icon(widget.iconData),
+        border: widget.border ?? OutlineInputBorder(),
+        focusedBorder: widget.focusedBorder,
+        enabledBorder: widget.enabledBorder,
+        errorBorder: widget.enabledBorder,
+        focusedErrorBorder: widget.focusedBorder,
+      ),
+      validator: widget.validator,
+    );
   }
 }
